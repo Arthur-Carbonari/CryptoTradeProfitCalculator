883f797f438bc2f08332c5ced954065160477fa9
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-10-22 21:28:45 +0100

    Connecting signals to slots to that a change in one control updates the UI

--- CalendarGroupBox.py
+++ CalendarGroupBox.py
@@ -1,7 +1,19 @@
+from PyQt6.QtCore import QDate, pyqtSignal
 from PyQt6.QtWidgets import QGroupBox, QLabel, QCalendarWidget, QSpinBox, QFormLayout, QSplitter, QHBoxLayout
 
 
 class CalendarGroupBox(QGroupBox):
+
+    purchase_quantity = 1
+    purchase_quantity_updated = pyqtSignal()
+
+    purchase_date: QDate
+    purchase_date_update = pyqtSignal()
+
+    sale_date: QDate
+    sale_date_update = pyqtSignal()
+
+    dates = []
 
     def __init__(self):
 
@@ -13,20 +25,25 @@ class CalendarGroupBox(QGroupBox):
         self.purchase_date_calendar.setDisabled(True)
 
         sell_date_label = QLabel("Date Sold:")
-        self.sell_date_calendar = QCalendarWidget()
-        self.sell_date_calendar.setDisabled(True)
+        self.sale_date_calendar = QCalendarWidget()
+        self.sale_date_calendar.setDisabled(True)
 
         # TODO: create QSpinBox to select CryptoCurrency quantity purchased
         quantity_purchased_label = QLabel("Quantity Purchased:")
-        quantity_purchased_spinbox = QSpinBox()
-        quantity_purchased_spinbox.setValue(1)
+        self.quantity_purchased_spinbox = QSpinBox()
+        self.quantity_purchased_spinbox.setValue(1)
+
+        # Connecting signals to slots to that a change in one control updates the UI
+        self.purchase_date_calendar.selectionChanged.connect(self._update_purchase_date)
+        self.sale_date_calendar.selectionChanged.connect(self._update_sale_date)
+        self.quantity_purchased_spinbox.valueChanged.connect(self._update_purchase_quantity)
 
         groupbox_purchase = QGroupBox("Purchase")
         groupbox_sell = QGroupBox("Sale")
 
         # Set purchase GroupBox layout
         purchase_layout = QFormLayout()
-        purchase_layout.addRow(quantity_purchased_label, quantity_purchased_spinbox)
+        purchase_layout.addRow(quantity_purchased_label, self.quantity_purchased_spinbox)
         purchase_layout.addRow(purchase_date_label, self.purchase_date_calendar)
 
         groupbox_purchase.setLayout(purchase_layout)
@@ -34,7 +51,7 @@ class CalendarGroupBox(QGroupBox):
         # Set sale GroupBox layout
         sale_layout = QFormLayout()
         # sale_layout.addRow(quantity_purchased_label, quantity_purchased_spinbox)
-        sale_layout.addRow(sell_date_label, self.sell_date_calendar)
+        sale_layout.addRow(sell_date_label, self.sale_date_calendar)
 
         groupbox_sell.setLayout(sale_layout)
 
